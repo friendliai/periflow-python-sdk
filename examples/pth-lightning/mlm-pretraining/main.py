87447bea2cc671951195d6f081e6089ec978c9ce
footprinthere <footprinthere@Tai-GRAM.localdomain>
2022-07-13 15:55:14 +0900

    Removed unnecessary code lines

--- examples/pth-lightning/mlm-pretraining/main.py
+++ examples/pth-lightning/mlm-pretraining/main.py
@@ -6,8 +6,6 @@ from lightning_transformers.task.nlp.masked_language_modeling import (
     MaskedLanguageModelingDataModule,
     MaskedLanguageModelingTransformer
 )
-from torch import distributed as torch_ddp
-from torch.distributed.elastic.multiprocessing.errors import record
 
 import periflow_sdk as pf
 import argparse
@@ -15,17 +13,12 @@ import os
 from tqdm import tqdm
 
 
-@record
 def main(args: argparse.Namespace):
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-    torch_ddp.init_process_group(backend='nccl')
 
     # Detect saved checkpoints
     if args.load_from_checkpoint:
         if args.checkpoint_dir and os.path.isdir(args.checkpoint_dir):
             # Load from the latest checkpoint file
-            print_once("### Finding checkpoint to initialize model")
             filenames = os.listdir(args.checkpoint_dir)
             if len(filenames) == 0:
                 raise RuntimeError("Specified checkpoint directory is empty.")
@@ -53,7 +46,6 @@ def main(args: argparse.Namespace):
         )
 
     # Prepare data module
-    print_once("### Preparing data module")
     num_train_examples = 36718
     dm = MaskedLanguageModelingDataModule(
         tokenizer=tokenizer,
@@ -92,7 +84,6 @@ def main(args: argparse.Namespace):
     pf.init(total_train_steps=total_train_steps)
 
     # Train
-    print_once("### Start training")
     trainer.fit(model, dm, ckpt_path=ckpt_path)
 
 
@@ -129,12 +120,6 @@ class PeriFlowTrainer(Trainer):
         pf.upload_checkpoint()
 
 
-def print_once(msg=''):
-    if not torch_ddp.is_initialized() or torch_ddp.get_rank() != 0:
-        return
-    print(msg, flush=True)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset-name", type=str, required=True)
@@ -150,11 +135,5 @@ if __name__ == '__main__':
     parser.add_argument("--log-interval", type=int, default=10)
     parser.add_argument("--lr", type=float, default=0.1)
     args = parser.parse_args()
-
-    # Print program arguments
-    print_once("#"*40)
-    print_once("INPUT ARGUMENTS")
-    for name, val in vars(args).items():
-        print_once(f"{name} = {val}")
-    print_once("#"*40)
+    
     main(args)
